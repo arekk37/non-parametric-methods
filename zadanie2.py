f5637115ae821dc14d82bfdc9437643fd094c3a4
arekk37 <66213427+arekk37@users.noreply.github.com>
2024-03-23 15:58:29 +0100

    Update zadanie2.py
    ukończone zadanie 2

--- zadanie2.py
+++ zadanie2.py
@@ -4,19 +4,19 @@ import matplotlib.pyplot as plt
 
 # generuje różne stopnie swobody dla prowadzonych testów, uznałem, że będą okej od 1 do 30, a potem 35 40 45 do 100.
 # w przypadku tego zadania testów będzie po 1000 by dokładniej sprawdzić czy są różnice pomiędzy klasycznym sposobem testowania a metodą PIT
-df = [i for i in range(1, 31, 10)] #+ [i for i in range(35, 101, 5)]
+df = [i for i in range(1, 31, 10)] + [i for i in range(35, 101, 5)]
 print(df)
 
 # teraz różne ilości danych uznalem, że okej będą 10, 20 i tak do 500
 
-SampleSize = [i for i in range(1, 100, 50)]
+SampleSize = [i for i in range(1, 101, 10)]
 print(SampleSize)
 
 
 # wyniki będę zapisywał w tablicy: [ [],[],[] ] czyli będzie się tutaj zapisywał wynik trzech testów.
 results = []
 counter = 0
-test_range = 1000
+test_range = 200
 #dodałem sobie ile ukonczylem testow zebym widzial ile zostalo do konca symulacji mniej wiecej
 total_tests = len(SampleSize) * len(df) * test_range
 current_tests  = 0
@@ -52,15 +52,16 @@ for size in SampleSize:
             chi2, p_chi2 = stats.chisquare(data)
 
          
-            # wyczytałem, że chi2 nie powinien być używany do jednostajnego rozkladu, a można to zrobićw taki sposób, że wg rozkładu który nam wyszedl przeliczamy to na zmienne dyskretne tzn dzielimy to na przedziły (ponoć dobrą metodą jest pierwiastek z liczebnosci proby) i liczymy ile z nich wpada do naszego przedziału i wtedy możemy obliczyć z łatwościa test 
+           # Obliczanie częstości dla przekształconych danych
             bins = np.linspace(0, 1, num=num_bins+1)
             counts, _ = np.histogram(data_pit, bins=bins)
-
-            # Obliczanie oczekiwanych częstości dla rozkładu jednostajnego
             expected_counts = np.full_like(counts, fill_value=len(data_pit)/num_bins)
 
-            chi2_pit, p_chi2_pit = stats.chisquare(counts, expected_counts)
+            # Normalizacja oczekiwanych częstości
+            expected_counts = expected_counts / np.sum(expected_counts) * np.sum(counts)
 
+            # Przeprowadzenie testu chi-kwadrat dla przekształconych danych
+            chi2_pit, p_chi2_pit = stats.chisquare(counts, expected_counts)
 
             # print("kologomorow: ", p_ks, " chi2: ", p_chi2)
 
